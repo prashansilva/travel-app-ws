bb1e2966de564c852558757162b211dca0678f60
PrashanSilva <prashansilva@gmail.com>
2020-08-03 14:53:50 +0530

    project initiated 2020/08/03

---
+++ src/main/java/com/techSolution/travelaagentappws/TravelaagentappWsApplication.java
@@ -0,0 +1,13 @@
+package com.techSolution.travelaagentappws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelaagentappWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelaagentappWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techSolution/travelaagentappws/TravelaagentappWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techSolution.travelaagentappws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelaagentappWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
